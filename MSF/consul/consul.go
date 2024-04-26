4efd920762d7f3cc20682f44a0767c87552b2df6
xiaol_deng1990 <xiaol_deng1990@163.com>
2024-04-26 20:48:30 +0800

    add init

--- MSF/consul/consul.go
+++ MSF/consul/consul.go
@@ -3,6 +3,8 @@ package consul
 import (
 	"fmt"
 	"sync"
+
+	"github.com/xiaolongdeng1990/forlife/MSF/config"
 )
 
 type ConsulConfig struct {
@@ -15,6 +17,24 @@ type ConsulUtils struct {
 
 var onceConusl sync.Once
 var ConsulInstance *ConsulUtils
+
+type SvrCfg struct {
+	Server struct {
+		ConsulAddr string `default:""`
+	}
+}
+
+func Init(cfg string) error {
+	svrCfg := SvrCfg{}
+
+	if err := config.ParseConfigWithPath(&svrCfg, cfg); err != nil {
+		fmt.Printf("load svrCfg failed. err:%+v cfg:%s", err, cfg)
+		return err
+	}
+
+	SetConsulAddr(svrCfg.Server.ConsulAddr)
+	return nil
+}
 
 func NewConsulUtils() *ConsulUtils {
 	onceConusl.Do(func() {
--- MSF/consul/go.mod
+++ MSF/consul/go.mod
@@ -1,3 +1,7 @@
 module github.com/xiaolongdeng1990/forlife/MSF/consul
 
 go 1.20
+
+require github.com/xiaolongdeng1990/forlife/MSF/config v0.0.0-20240423133039-064980ade61d
+
+require github.com/BurntSushi/toml v1.3.2 // indirect
